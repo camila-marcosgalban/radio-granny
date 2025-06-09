f6ffbad6607f45c85a016116eb794c076c3c94f6
mila <milamarcos98@gmail.com>
2025-06-09 17:48:12 -0300

    radio

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import RadioApp from './components/RadioApp'
+
+function App() {
+
+  return (
+    <>
+    <RadioApp/>
+    </>
+  )
+}
+
+export default App
